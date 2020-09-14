793a341f6d2acf0aa0038ba21f723d368f2274a4
Sergey <tokarev7557@gmail.com>
2020-09-14 17:17:01 +0300

    Updates

---
+++ precache-manifest.ccbbb2d1c1f4aa6dc9542647b43fa642.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14589d509a3077f1add9336bd862cdb",
+    "url": "/ED4M/CRUD-React-TestTask/index.html"
+  },
+  {
+    "revision": "0a5209de6330f79b7baa",
+    "url": "/ED4M/CRUD-React-TestTask/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "72d7de2015736aba7889",
+    "url": "/ED4M/CRUD-React-TestTask/static/css/main.b5e697d3.chunk.css"
+  },
+  {
+    "revision": "0a5209de6330f79b7baa",
+    "url": "/ED4M/CRUD-React-TestTask/static/js/2.7f4be047.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ED4M/CRUD-React-TestTask/static/js/2.7f4be047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d7de2015736aba7889",
+    "url": "/ED4M/CRUD-React-TestTask/static/js/main.37b958c2.chunk.js"
+  },
+  {
+    "revision": "8cb7e61d69e114ad72ac",
+    "url": "/ED4M/CRUD-React-TestTask/static/js/runtime-main.0dff602e.js"
+  }
+]);
